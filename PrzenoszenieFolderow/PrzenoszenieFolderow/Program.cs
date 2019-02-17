9a6b8c2965a1938c2e62edfa2efe40bb20e76a29
KamilSulich <44139050+KamilSulich@users.noreply.github.com>
2019-02-17 23:29:20 +0100

    Add files via upload
    Stworzenie repo

---
+++ PrzenoszenieFolderow/PrzenoszenieFolderow/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PrzenoszenieFolderow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FolderPrzenoszony = @"C:\dane";
+            string gdzie = @"C:\dane2";
+
+            try
+            {
+                Directory.Move(FolderPrzenoszony, gdzie);
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                Directory.CreateDirectory(gdzie);
+                Directory.Move(FolderPrzenoszony, gdzie);
+            }
+        }
+    }
+}
